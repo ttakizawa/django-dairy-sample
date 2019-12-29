e0d6e35057e3c0a37b0a2c062d1f489e426368b0
ttakizawa <zawa78@gmail.com>
2019-12-29 15:17:00 +0900

    #7

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -118,7 +118,7 @@ SITE_ID = 1
 
 AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend', # 一般ユーザー用（メールアドレス認証）
-    'django.contrib.auth.backend.ModelBackend', # 管理サイト用（ユーザー名認証）
+    'django.contrib.auth.backends.ModelBackend', # 管理サイト用（ユーザー名認証）
 )
 
 # メールアドレス認証に変更する設定
@@ -130,7 +130,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
 
 # ログイン/ログアウト後の遷移先を設定
-LOGIN_REDIRECT_URL = 'diary:index'
+LOGIN_REDIRECT_URL = 'diary:diary_list'
 ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
 
 # ログアウトリンクのクリック一発でログアウトする設定
@@ -206,3 +206,5 @@ MESSAGE_TAGS = {
     messages.SUCCESS: 'alert alert-success',
     messages.INFO: 'alert alert-info',
 }
+
+MEDIA_URL = '/media/'
